2d5280e543acfb24469da6c418effec2b57f30a5
offmihay <offmihay@gmail.com>
2025-01-12 20:30:02 +0200

    Fix: Custom Dropdown

--- src/components/shared/dropdown/CustomDropdown.tsx
+++ src/components/shared/dropdown/CustomDropdown.tsx
@@ -1,27 +1,15 @@
-// import * as DropdownMenu from "zeego/dropdown-menu";
+import * as DropdownMenu from "zeego/dropdown-menu";
 
-// export const DropdownMenuRoot = DropdownMenu.Root;
+export const DropdownMenuRoot = DropdownMenu.Root;
 
-// export const DropdownMenuContent = DropdownMenu.Content;
-// export const DropdownMenuTrigger = DropdownMenu.Trigger;
-// export const DropdownMenuItemTitle = DropdownMenu.ItemTitle;
-// export const DropdownMenuCheckboxItem = DropdownMenu.CheckboxItem;
-// export const DropdownMenuItemIndicator = DropdownMenu.ItemIndicator;
+export const DropdownMenuContent = DropdownMenu.Content;
+export const DropdownMenuTrigger = DropdownMenu.Trigger;
+export const DropdownMenuItemTitle = DropdownMenu.ItemTitle;
+export const DropdownMenuCheckboxItem = DropdownMenu.CheckboxItem;
+export const DropdownMenuItemIndicator = DropdownMenu.ItemIndicator;
 
-// type ItemProps = React.ComponentProps<(typeof DropdownMenu)["Item"]>;
+type ItemProps = React.ComponentProps<(typeof DropdownMenu)["Item"]>;
 
-// export const DropdownMenuItem = DropdownMenu.create((props: ItemProps) => {
-//   return <DropdownMenu.Item {...props} />;
-// }, "Item");
-
-const StubComponent: React.FC<any> = () => {
-  return null;
-};
-
-export const DropdownMenuRoot = StubComponent;
-export const DropdownMenuContent = StubComponent;
-export const DropdownMenuTrigger = StubComponent;
-export const DropdownMenuItemTitle = StubComponent;
-export const DropdownMenuCheckboxItem = StubComponent;
-export const DropdownMenuItemIndicator = StubComponent;
-export const DropdownMenuItem = StubComponent;
+export const DropdownMenuItem = DropdownMenu.create((props: ItemProps) => {
+  return <DropdownMenu.Item {...props} />;
+}, "Item");
