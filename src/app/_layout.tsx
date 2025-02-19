24fd7dc64936e2a04d827db00ef23b65e5a5c381
offmihay <offmihay@gmail.com>
2025-02-19 02:32:17 +0200

    changed adaptive-icon & tabbars color fixes

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -7,6 +7,9 @@ import { Slot, Stack } from "expo-router";
 import React, { PropsWithChildren, useEffect } from "react";
 import * as SplashScreen from "expo-splash-screen";
 import { useAuthContext } from "../providers/AuthContextProvider";
+import { useCustomTheme } from "@/src/hooks/useCustomTheme";
+import * as NavigationBar from "expo-navigation-bar";
+import { Platform, StatusBar } from "react-native";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -17,13 +20,28 @@ SplashScreen.setOptions({
 
 const Layout = ({ children }: PropsWithChildren) => {
   const { isLoading } = useAuthContext();
+  const theme = useCustomTheme();
 
   useEffect(() => {
     if (isLoading) return;
     SplashScreen.hide();
   }, [isLoading]);
 
-  return children;
+  useEffect(() => {
+    if (Platform.OS === "android") {
+      NavigationBar.setBackgroundColorAsync(theme.colors.background);
+    }
+  }, [theme.colors.background]);
+
+  return (
+    <>
+      <StatusBar
+        backgroundColor={theme.colors.background}
+        barStyle={theme.dark ? "light-content" : "dark-content"}
+      />
+      {children}
+    </>
+  );
 };
 
 const RootLayout = () => {
